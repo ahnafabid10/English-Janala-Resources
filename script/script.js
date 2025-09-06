3bf93182d7bad7adaa5fc2b702714a907d0badef
Ahnaf Abid <ahnafabid600@gmail.com>
2025-09-07 00:04:26 +0600

    first commit

--- script/script.js
+++ script/script.js
@@ -4,6 +4,39 @@ const loadLessons =() =>{
     .then(json => displayLesson(json.data))
 }
 
+const loadLevelWord = (id) => {
+    console.log(id)
+    const url =`https://openapi.programming-hero.com/api/level/${id}`;
+    console.log(url);
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayLevelWord(data.data))
+}
+
+const displayLevelWord =(words) => {
+    console.log(words)
+    const wordContainer = document.getElementById('word-container')
+    wordContainer.innerHTML = "";
+    words.forEach(word => {
+        console.log(word)
+    const card = document.createElement("div")
+    card.innerHTML =`
+    <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+      <h2 class="font-bold text-2xl ">${word.word}</h2>
+      <p class="font-semibold">Meaning /Pronounciation</p>
+      <div class="font-bangla text-medium text-2xl">"${word.meaning} / ${word.pronunciation}" </div>
+      <div class="flex justify-between items-center">
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+        <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+
+      </div>
+    </div>
+    `
+    wordContainer.append(card)
+    });
+
+}
+
 const displayLesson = (lessons) => {
     // console.log(lessons);
 
@@ -15,7 +48,7 @@ const displayLesson = (lessons) => {
     // 3. Create Element 
     const btnDiv = document.createElement('div')
     btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button >
+        <button onClick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"><i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}</button >
     `;
     // 4. Append into container 
     levelContainer.append(btnDiv);
